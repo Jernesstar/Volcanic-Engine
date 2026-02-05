2a8fa72021bcda1f2d6b7b30d894ed5d8c54d55e
Jercy Mukala <jercymukala@gmail.com>
2026-02-05 16:33:58 -0500

    Let the games begin!

--- Magma/assets/scripts/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set componentPath=%1
-cd /D %componentPath%
-cd "Build\Platform\gcc-Windows"
-mingw32-make.exe -f Makefile
--- Magma/src/Editor/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <VolcaniCore/Core/CommandLineArgs.h>
-
-#include "Core/EditorApp.h"
-
-Application* CreateApplication(const CommandLineArgs& args) {
-	return new Magma::EditorApp(args);
-}
